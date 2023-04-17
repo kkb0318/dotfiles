0cbade9b14ed6fd93279d73ff98c495227c5053e
kkb0318 <smallobuti@gmail.com>
2023-04-17 14:43:11 +0900

    init mason settings

--- nvim/after/plugin/mason.rc.lua
+++ nvim/after/plugin/mason.rc.lua
@@ -3,10 +3,40 @@ if (not status) then return end
 local status2, lspconfig = pcall(require, "mason-lspconfig")
 if (not status2) then return end
 
-mason.setup({
 
+
+local lsp_servers = {
+  "gopls", "tsserver", "pylsp", "yamlls", "cssls", "jsonls", "clangd", "terraformls", "rust_analyzer", "lua_ls"
+}
+
+local tools = {
+  -- Formatter
+  "black",
+  "flake8",
+  "isort",
+  "prettier",
+  "jq",
+  -- Linter
+  "rstcheck",
+  "eslint_d",
+  "jsonlint",
+}
+
+mason.setup({
 })
 
 lspconfig.setup {
-  automatic_installation = true
+  automatic_installation = true,
+  ensure_installed = lsp_servers
 }
+
+
+
+-- ensure tools (except LSPs) are installed
+local mr = require("mason-registry")
+for _, tool in ipairs(tools) do
+  local p = mr.get_package(tool)
+  if not p:is_installed() then
+    p:install()
+  end
+end
