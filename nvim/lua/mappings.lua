f3738b31725ffc1a4fc77d2267c263fa4889b67d
kkb0318 <smallobuti@gmail.com>
2023-08-29 10:07:18 +0900

    add help ja

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -5,8 +5,6 @@ keymap.set('n', 'x', '"_x')
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
--- New tab
-keymap.set('n', 'te', ':tabedit<CR>')
 
 -- Split window
 keymap.set('n', 'ss', ':split<Return><C-w>w')
@@ -28,3 +26,32 @@ keymap.set('n', '<C-w><down>', '<C-w>-')
 -- no highlight
 keymap.set("n", "<Esc><Esc>", "<Cmd>nohl<CR>")
 
+
+-- New tab
+keymap.set('n', 'tn', ':tabnew<CR>')
+
+
+
+-- Move current page to New tab
+keymap.set("n", "tm", [[<Cmd>lua MoveToNewTab()<CR>]], { silent = true })
+
+function MoveToNewTab()
+  -- Equivalent to :tab split
+  vim.cmd("tab split")
+  -- Equivalent to :tabprevious
+  vim.cmd("tabprevious")
+  -- Get window count in the current tab page
+  local win_count = vim.fn.winnr('$')
+  -- Get buffer count
+  local buf_count = vim.fn.bufnr('$')
+  -- Equivalent to Vimscript's if-elseif-endif logic
+  if win_count > 1 then
+    -- Equivalent to :close
+    vim.cmd("close")
+  elseif buf_count > 1 then
+    -- Equivalent to :buffer #
+    vim.cmd("buffer #")
+  end
+  -- Equivalent to :tabnext
+  vim.cmd("tabnext")
+end
