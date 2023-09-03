17a75f27c0f41267dd150941f8f5fc45d284fb34
kkb0318 <smallobuti@gmail.com>
2023-09-03 12:34:06 +0900

    add ai-review plugin

--- nvim/filetype.lua
+++ nvim/filetype.lua
@@ -4,34 +4,45 @@
 -- Enable neovim runtime filetype.lua
 vim.g.do_filetype_lua = 1
 
-vim.filetype.add({
+-- Define the mappings in a table
+local mappings = {
+  -- Filename mappings
+  {
     filename = {
-        Brewfile = 'ruby',
-        justfile = 'just',
-        Justfile = 'just',
-        Tmuxfile = 'tmux',
-        ['go.sum'] = 'go',
-        ['yarn.lock'] = 'yaml',
-        ['.buckconfig'] = 'toml',
-        ['.flowconfig'] = 'ini',
-        ['.tern-project'] = 'json',
-        ['.jsbeautifyrc'] = 'json',
-        ['.jscsrc'] = 'json',
-        ['.watchmanconfig'] = 'json',
+      Brewfile = 'ruby',
+      justfile = 'just',
+      Justfile = 'just',
+      Tmuxfile = 'tmux',
+      ['go.sum'] = 'go',
+      ['yarn.lock'] = 'yaml',
+      ['.buckconfig'] = 'toml',
+      ['.flowconfig'] = 'ini',
+      ['.tern-project'] = 'json',
+      ['.jsbeautifyrc'] = 'json',
+      ['.jscsrc'] = 'json',
+      ['.watchmanconfig'] = 'json',
     },
+  },
+  -- Pattern mappings
+  {
     pattern = {
-        ['.*%.js%.map'] = 'json',
-        ['.*%.postman_collection'] = 'json',
-        ['Jenkinsfile.*'] = 'groovy',
-        ['%.kube/config'] = 'yaml',
-        ['%.config/git/users/.*'] = 'gitconfig',
-        ['.*/templates/.*%.yaml'] = 'helm',
-        ['.*/templates/.*%.yml'] = 'helm',
-        ['.*/templates/.*%.tpl'] = 'helm',
-        ['.*/playbooks/.*%.yaml'] = 'yaml.ansible',
-        ['.*/playbooks/.*%.yml'] = 'yaml.ansible',
-        ['.*/roles/.*%.yaml'] = 'yaml.ansible',
-        ['.*/roles/.*%.yml'] = 'yaml.ansible',
-        ['.*/inventory/.*%.ini'] = 'ansible_hosts',
+      ['.*%.js%.map'] = 'json',
+      ['.*%.postman_collection'] = 'json',
+      ['Jenkinsfile.*'] = 'groovy',
+      ['%.kube/config'] = 'yaml',
+      ['%.config/git/users/.*'] = 'gitconfig',
+      ['.*/templates/.*%.yaml'] = 'helm',
+      ['.*/templates/.*%.yml'] = 'helm',
+      ['.*/templates/.*%.tpl'] = 'helm',
+      ['.*/playbooks/.*%.yaml'] = 'yaml.ansible',
+      ['.*/playbooks/.*%.yml'] = 'yaml.ansible',
+      ['.*/roles/.*%.yaml'] = 'yaml.ansible',
+      ['.*/roles/.*%.yml'] = 'yaml.ansible',
+      ['.*/inventory/.*%.ini'] = 'ansible_hosts',
     },
-})
+  },
+}
+
+for _, mapping in ipairs(mappings) do
+  vim.filetype.add(mapping)
+end
---
+++ nvim/lua/rc/plugins/ddu/ai-review.lua
@@ -0,0 +1,23 @@
+local helper = require("helpers.ddu")
+
+---@type LazySpec
+local spec = {
+  "yuki-yano/ai-review.vim",
+  dependencies = { "ddu.vim" },
+  config = function()
+helper.patch_global({
+  kindOptions= {
+    ["ai-review-request"]={
+      defaultAction= "open",
+    },
+    ["ai-review-log"]= {
+      defaultAction= "resume",
+    },
+    }
+  })
+    vim.fn["ai_review#config"]({ chat_gpt = { model = "gpt-3.5-turbo" } })
+  end,
+}
+
+return spec
+
