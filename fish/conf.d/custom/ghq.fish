aa755d9e6e1b885f93d8f15d41521a3ff105eb34
kkb0318 <smallobuti@gmail.com>
2023-09-25 13:25:48 +0900

    add fish pr

--- fish/conf.d/custom/ghq.fish
+++ fish/conf.d/custom/ghq.fish
@@ -1,4 +1,4 @@
-bind \cgi '__ghq_repository_search'
+bind \cgr '__ghq_repository_search'
 if bind -M insert >/dev/null 2>/dev/null
-    bind -M insert \cgi '__ghq_repository_search'
+    bind -M insert \cgr '__ghq_repository_search'
 end
--- fish/conf.d/custom/github_functions.fish
+++ fish/conf.d/custom/github_functions.fish
@@ -1,10 +1,19 @@
 function show_github_issue
     set selected (gh issue list --state open --limit 100 | fzf --preview 'gh issue view (echo {} | cut -f1)' | cut -f1)
     if test -n "$selected"
-        set -l BUFFER "gh issue view $selected"
+        set -l BUFFER "gh issue view -w $selected"
         eval $BUFFER
     end
 end
 
+function show_github_pr
+    set selected (gh pr list --state open --limit 100 | fzf --preview 'gh pr view (echo {} | cut -f1)' | cut -f1)
+    if test -n "$selected"
+        set -l BUFFER "gh pr view -w $selected"
+        eval $BUFFER
+    end
+end
+
+bind \cgp show_github_pr
 bind \cgi show_github_issue
 
