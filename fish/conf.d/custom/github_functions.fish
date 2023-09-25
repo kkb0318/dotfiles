d7ff57046b1694d15522d1c1f9d09740e9e09609
kkb0318 <smallobuti@gmail.com>
2023-09-25 13:13:06 +0900

    fix fish config

--- fish/conf.d/custom/github_functions.fish
+++ fish/conf.d/custom/github_functions.fish
@@ -1,5 +1,5 @@
 function show_github_issue
-    set selected (gh issue list --state open --limit 100 | fzf --preview 'gh issue view -p (echo {} | cut -f1)' | cut -f1)
+    set selected (gh issue list --state open --limit 100 | fzf --preview 'gh issue view (echo {} | cut -f1)' | cut -f1)
     if test -n "$selected"
         set -l BUFFER "gh issue view $selected"
         eval $BUFFER
