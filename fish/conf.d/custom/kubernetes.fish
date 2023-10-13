0e5a9eac79a210736e2044e8e449f2da89f609c4
kkb0318 <smallobuti@gmail.com>
2023-10-13 22:52:34 +0900

    fix fish config for k8s

--- fish/conf.d/custom/kubernetes.fish
+++ fish/conf.d/custom/kubernetes.fish
@@ -6,5 +6,44 @@ function switch-k8s-context
     end
 end
 
+# Helper function to get a selected resource using peco
+function get-selected-resource
+    set resource_type $argv[1]
+    echo (kubectl get $resource_type -A | peco)
+end
+
+# Helper function to reconcile a resource using Flux
+function reconcile-resource
+    set resource_type $argv[1]
+    set selected $argv[2]
+
+    # Extract namespace and name from the selected line
+    set namespace (echo $selected | awk '{print $1}')
+    set name (echo $selected | awk '{print $2}')
+
+    # Check if both namespace and name are set
+    if test -n "$namespace" -a -n "$name"
+        # Reconcile the resource using Flux
+        flux reconcile $resource_type $name -n $namespace
+    else
+        echo "No $resource_type selected."
+        commandline -f repaint
+    end
+end
+
+
+function reconcile-hr
+    set selected (get-selected-resource hr)
+    reconcile-resource hr $selected
+end
+
+function reconcile-ks
+    set selected (get-selected-resource ks)
+    reconcile-resource ks $selected
+end
+
+
 bind \ck switch-k8s-context
+bind \ckhr reconcile-hr
+bind \ckks reconcile-ks
 
