4b40e117a1d7bdc436ce0942ec6f3726de502568
kkb0318 <smallobuti@gmail.com>
2023-11-04 23:04:45 +0900

    fix gh pr keybind

--- fish/conf.d/custom/github_functions.fish
+++ fish/conf.d/custom/github_functions.fish
@@ -9,7 +9,9 @@ end
 function show_github_pr
     set selected (gh pr list --state open --limit 100 | fzf --preview 'gh pr view (echo {} | cut -f1)' | cut -f1)
     if test -n "$selected"
+        set -l CO_BUFFER "gh pr checkout $selected"
         set -l BUFFER "gh pr view -w $selected"
+        eval $CO_BUFFER
         eval $BUFFER
     end
 end
