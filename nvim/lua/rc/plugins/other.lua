2ce9fdec1f4d2869535779aa8db10ffb5ffbbf9d
kkb0318 <smallobuti@gmail.com>
2024-04-15 10:30:00 +0900

    add sonic template

--- nvim/lua/rc/plugins/other.lua
+++ nvim/lua/rc/plugins/other.lua
@@ -21,6 +21,23 @@ local spec = {
   {
     "danymat/neogen", -- doc generator
   },
+  {
+    "mattn/vim-sonictemplate",
+    cmd = { "Template" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      vim.g.sonictemplate_key = 0
+      vim.g.sonictemplate_intelligent_key = 0
+      vim.g.sonictemplate_postfix_key = 0
+      local Path = require("plenary.path")
+      vim.g.sonictemplate_vim_template_dir = {
+        Path:new(vim.fn.stdpath("config"), "template"):absolute(),
+      }
+    end,
+    init = function()
+      vim.keymap.set("n", "<leader>t", ":Template ")
+    end,
+  }
 }
 
 return spec
