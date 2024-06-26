5ba6e0617749aee172675338309d3270a6b30ba0
kkb0318 <smallobuti@gmail.com>
2024-06-26 10:47:17 +0900

    fix color scheme

--- nvim/lua/rc/plugins/color.lua
+++ nvim/lua/rc/plugins/color.lua
@@ -1,23 +1,29 @@
 ---@type LazySpec
 local spec = {
   {
-    "bluz71/vim-nightfly-colors",
-    name = "nightfly",
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.g.nightflyItalics = false
-      vim.cmd.colorscheme("nightfly")
-
-      local palette = require("nightfly").palette
-
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = palette.black })
-      vim.api.nvim_set_hl(0, "FloatBorder", { fg = palette.white, bg = palette.black })
-      vim.api.nvim_set_hl(0, "PmenuSel", { bg = palette.cyan_blue })
-      vim.api.nvim_set_hl(0, "LspInlayHint", { link = "Comment" })
+      vim.cmd.colorscheme("catppuccin-macchiato")
     end,
-
   },
+  -- {
+  --   "bluz71/vim-nightfly-colors",
+  --   name = "nightfly",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.g.nightflyItalics = false
+  --     vim.cmd.colorscheme("nightfly")
+  --     local palette = require("nightfly").palette
+  --
+  --     vim.api.nvim_set_hl(0, "NormalFloat", { bg = palette.black })
+  --     vim.api.nvim_set_hl(0, "FloatBorder", { fg = palette.white, bg = palette.black })
+  --     vim.api.nvim_set_hl(0, "PmenuSel", { bg = palette.cyan_blue })
+  --     vim.api.nvim_set_hl(0, "LspInlayHint", { link = "Comment" })
+  --   end,
+  -- },
 }
 
 return spec
--- nvim/lua/rc/plugins/ddu/filters/fzf.lua
+++ nvim/lua/rc/plugins/ddu/filters/fzf.lua
@@ -5,11 +5,8 @@ local spec = {
   "yuki-yano/ddu-filter-fzf",
   dependencies = {
     "ddu.vim",
-    "bluz71/vim-nightfly-colors",
   },
   config = function()
-    local palette = require("nightfly").palette
-    vim.api.nvim_set_hl(0, "DduSearchMatched", { fg = palette.black, bg = palette.emerald })
     helper.patch_global({
       sourceOptions = {
         _ = {
