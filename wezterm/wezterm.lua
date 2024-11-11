27ae586d808783d2969ee7e184cba3b1e57949b6
kkb0318 <smallobuti@gmail.com>
2024-11-11 21:40:08 +0900

    fix wezterm settings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,4 @@
 local wezterm = require("wezterm")
-local utils = require("utils")
 local keybinds = require("keybinds")
 local scheme = wezterm.get_builtin_color_schemes()["nord"]
 local gpus = wezterm.gui.enumerate_gpus()
@@ -7,56 +6,9 @@ require("on")
 
 
 
----------------------------------------------------------------
---- Merge the Config
----------------------------------------------------------------
-local function create_ssh_domain_from_ssh_config(ssh_domains)
-  if ssh_domains == nil then
-    ssh_domains = {}
-  end
-  for host, config in pairs(wezterm.enumerate_ssh_hosts()) do
-    table.insert(ssh_domains, {
-      name = host,
-      remote_address = config.hostname .. ":" .. config.port,
-      username = config.user,
-      multiplexing = "None",
-      assume_shell = "Posix",
-    })
-  end
-  return { ssh_domains = ssh_domains }
-end
-
 --- load local_config
 -- Write settings you don't want to make public, such as ssh_domains
 package.path = os.getenv("HOME") .. "/.local/share/wezterm/?.lua;" .. package.path
-local function load_local_config(module)
-  local m = package.searchpath(module, package.path)
-  if m == nil then
-    return {}
-  end
-  return dofile(m)
-  -- local ok, _ = pcall(require, "local")
-  -- if not ok then
-  -- 	return {}
-  -- end
-  -- return require("local")
-end
-
-local local_config = load_local_config("local")
-
--- local local_config = {
--- 	ssh_domains = {
--- 		{
--- 			-- This name identifies the domain
--- 			name = "my.server",
--- 			-- The address to connect to
--- 			remote_address = "192.168.8.31",
--- 			-- The username to use on the remote host
--- 			username = "katayama",
--- 		},
--- 	},
--- }
--- return local_config
 
 ---------------------------------------------------------------
 --- Config
@@ -94,6 +46,9 @@ local config = {
   hide_tab_bar_if_only_one_tab = false,
   adjust_window_size_when_changing_font_size = false,
   selection_word_boundary = " \t\n{}[]()\"'`,;:│=&!%",
+
+  hide_tab_bar_if_only_one_tab = true,
+  window_decorations = "RESIZE",
   window_padding = {
     left = 0,
     right = 0,
@@ -113,9 +68,17 @@ local config = {
     },
   },
   exit_behavior = "CloseOnCleanExit",
+  ----------------------------------------------------
+  -- Tab
+  ----------------------------------------------------
   tab_bar_at_bottom = false,
+  show_new_tab_button_in_tab_bar = false,
+  show_close_tab_button_in_tabs = false,
+
+  -- window settings
   window_close_confirmation = "AlwaysPrompt",
-  -- window_background_opacity = 0.8,
+  window_background_opacity = 0.85,
+  macos_window_background_blur = 20,
   disable_default_key_bindings = true,
   -- visual_bell = {
   -- 	fade_in_function = "EaseIn",
@@ -136,5 +99,4 @@ local config = {
   native_macos_fullscreen_mode = true,
 }
 
-local merged_config = utils.merge_tables(config, local_config)
-return utils.merge_tables(merged_config, create_ssh_domain_from_ssh_config(merged_config.ssh_domains))
+return config
