c26471228d64c58347558da3c7e1d9cc3e5f4c33
kkb0318 <smallobuti@gmail.com>
2025-10-08 18:24:17 +0900

    fix: ts ls

--- nvim/after/lsp/ts_ls.lua
+++ nvim/after/lsp/ts_ls.lua
@@ -1,5 +1,6 @@
 local _, util = pcall(require, "lspconfig/util")
-local config = {
+
+return {
   root_dir = util.root_pattern("package.json"),
   filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
   cmd = { "typescript-language-server", "--stdio" },
@@ -8,11 +9,5 @@ local config = {
     -- ts_lsのフォーマット機能を無効化（on_attachより先に実行される）
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
-  end,
-}
-
-return {
-  config = function(_)
-    return config
   end,
 }
