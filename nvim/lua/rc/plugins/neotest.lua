bccc034826c3c57d9d6f4b744f88368818a5f70c
kkb0318 <smallobuti@gmail.com>
2026-04-08 15:52:56 +0900

    delete treesitter

--- nvim/lua/rc/plugins/neotest.lua
+++ nvim/lua/rc/plugins/neotest.lua
@@ -5,7 +5,6 @@ local spec = {
       "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter",
       "nvim-neotest/neotest-python",
       'nvim-neotest/neotest-go',
     },
